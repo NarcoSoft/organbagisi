b73cb0ef9aa8b7bcdf7bddb5ae528519acccf5fe
Narcode Software <muhammettyazar42@gmail.com>
2024-09-11 00:00:20 +0300

    duzenleme kurulu aktif

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-=======
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
->>>>>>> c83c54f (Commit mesajınız)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-=======
-  server: {
-    host: '0.0.0.0',
-  }
->>>>>>> c83c54f (Commit mesajınız)
 })
