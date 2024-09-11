36273f25dea274f6e0b48d338e9b8678adeac306
Narcode Software <muhammettyazar42@gmail.com>
2024-09-11 10:43:53 +0300

    Formda Anasayfaya yönlendirme butonu eklendi

--- src/Pages/BasvuruFormu/basvuru_formu.jsx
+++ src/Pages/BasvuruFormu/basvuru_formu.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './basvuru_formu.css'
 
 const BasvuruFormu = () => {
@@ -5,7 +6,11 @@ const BasvuruFormu = () => {
     <div className="form-body">
       <div className="container">
         <div className="d-flex">
-          <iframe className="d-flex mx-auto my-4" src="https://docs.google.com/forms/d/e/1FAIpQLScJsjodL6-oKPgIO7iLj2GmR1CUP-oT9gBIe2eAOspnq612Fw/viewform?embedded=true" width="640" height="520" frameborder="0" marginheight="0" marginwidth="0">Yükleniyor…</iframe>
+          <iframe className="d-flex mx-auto my-4" src="https://docs.google.com/forms/d/e/1FAIpQLScJsjodL6-oKPgIO7iLj2GmR1CUP-oT9gBIe2eAOspnq612Fw/viewform?embedded=true" width="640" height="520" frameborder="0">Yükleniyor…</iframe>
+          
+        </div>
+        <div className='d-flex'>
+          <Link className=' mx-auto mb-2' to={'/'}><button className='btn btn-primary'>Anasayfa'ya Dön</button></Link>
         </div>
     </div>
     </div>
