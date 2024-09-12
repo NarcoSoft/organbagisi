79ba5bdbdf4000b460f99ea879ea17e1d9c60465
Narcode Software <muhammettyazar42@gmail.com>
2024-09-12 23:28:55 +0300

    formmm

--- src/Pages/BasvuruFormu/basvuru_formu.jsx
+++ src/Pages/BasvuruFormu/basvuru_formu.jsx
@@ -15,21 +15,21 @@ const BasvuruFormu = () => {
       <div className="container">
         <div className="d-flex">
           
-        <iframe
-          id="JotFormIFrame-242554616412049"
-          title="Organ Bağışı Slogan Yarışması"
-          onload="window.parent.scrollTo(0,0)"
-          allowtransparency="true"
-          allow="geolocation; microphone; camera; fullscreen"
-          src="https://form.jotform.com/242554616412049"
-          frameborder="0"
-          style={{minWidth: '100%', maxWidth: '100%', height: '640px', border: 'none'}}
-          scrolling="yes"
-          className='mt-2 mb-4'
-        >
-        </iframe>
-        <script src='https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js'></script>
-        <script>window.jotformEmbedHandler("iframe[id='JotFormIFrame-242554616412049']", "https://form.jotform.com/")</script>
+    <iframe
+      id="JotFormIFrame-242552995659070"
+      title="Başvuru Formu"
+      onload="window.parent.scrollTo(0,0)"
+      allowtransparency="true"
+      allow="geolocation; microphone; camera; fullscreen"
+      src="https://form.jotform.com/242552995659070"
+      frameborder="0"
+      scrolling="yes"
+      style={{minWidth: '100%', maxWidth: '100%', height: '539px', border: 'none'}}
+      className='my-4'
+    >
+    </iframe>
+    <script src='https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js'></script>
+    <script>window.jotformEmbedHandler("iframe[id='JotFormIFrame-242552995659070']", "https://form.jotform.com/")</script>
     
         </div>
         <div className='d-flex'>
@@ -39,6 +39,5 @@ const BasvuruFormu = () => {
     </div>
   )
 }
-
 
 export default BasvuruFormu;
