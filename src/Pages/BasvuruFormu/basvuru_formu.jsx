6143f1eabc40729d2baad716094795d4db2318d5
Narcode Software <muhammettyazar42@gmail.com>
2024-09-30 12:16:31 +0300

    sfd

--- src/Pages/BasvuruFormu/basvuru_formu.jsx
+++ src/Pages/BasvuruFormu/basvuru_formu.jsx
@@ -18,7 +18,7 @@ const BasvuruFormu = () => {
     <div className="form-body">
       <div className="container">
         <div className="d-flex">  
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfV-r_fMRhguUI1vMUbwz0Eg34sIxrlShuiK5qvCkJcBsffQg/viewform?embedded=true" width="640" height="600" className='mx-auto'>Yükleniyor…</iframe>      
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfV-r_fMRhguUI1vMUbwz0Eg34sIxrlShuiK5qvCkJcBsffQg/viewform?embedded=true" width="640" height="560" className='mx-auto mt-3'>Yükleniyor…</iframe>      
         </div>
         <div className='d-flex'>
           <Link className=' mx-auto my-4' to={'/'}><button className='btn btn-primary'>Anasayfa'ya Dön</button></Link>
